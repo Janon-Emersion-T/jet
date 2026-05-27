4b7496de97b8c585cb3932371b4f68bbe04ebd49
Janon-Emersion-T <janonemersion@hotmail.com>
2026-05-27 23:33:29 +0530

    Add NLP-000O multi-intent command parser

--- core/nlp_engine.py
+++ core/nlp_engine.py
@@ -1,2 +1,3 @@
 from core.nlp.phase000_engine import analyze_command, classify_intent_nlp, format_nlp_report
 from core.nlp.multi_intent_parser import parse_multi_intent_command, format_multi_intent_report
+from core.nlp.command_execution_planner import build_execution_plan, format_execution_plan
